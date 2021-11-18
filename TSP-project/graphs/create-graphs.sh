196159a11e70e74732a7c785a44d402ff35d4ae5
kevin <kevin@harrisoncentral.com>
2021-11-18 14:46:40 +0100

    Added name of CLI tool (GraphViz)

--- TSP-project/graphs/create-graphs.sh
+++ TSP-project/graphs/create-graphs.sh
@@ -3,5 +3,6 @@
 # Find files ending with .dot extension
 for i in $(find . -name '*.dot' ); 
 do
+    # Using graphviz CLI tool
     neato -Tpng $i -o "${i%.dot}.png" -n # Create PNG from .dot file
 done

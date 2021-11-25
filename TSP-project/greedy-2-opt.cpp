019b7c77b1ca864fa7db78891034988134d5f4c2
Erik Henriksson <erik.ch.henriksson@gmail.com>
2021-11-25 22:43:14 +0100

    working on euler path

--- TSP-project/greedy-2-opt.cpp
+++ TSP-project/greedy-2-opt.cpp
@@ -12,50 +12,54 @@
 using std::tuple;
 using namespace std;
 
-void print_vector(vector<int>& vect){
-    for(auto i: vect){
+void print_vector(vector<int> &vect)
+{
+    for (auto i : vect)
+    {
         cout << i << ", ";
     }
     cout << endl;
 }
 
-
 // Rounded Euclidean distance
-double dist(int city_a, int city_b, vector<pair<double, double> >& cities)
+double dist(int city_a, int city_b, vector<pair<double, double>> &cities)
 {
-    return round(sqrt(pow(cities[city_a].first  - cities[city_b].first, 2) +
+    return round(sqrt(pow(cities[city_a].first - cities[city_b].first, 2) +
                       pow(cities[city_a].second - cities[city_b].second, 2)));
 }
 
-
 // Calculate the distance of an entire tour
-double total_distance(vector<int>& tour, vector<pair<double, double> >& cities) {
+double total_distance(vector<int> &tour, vector<pair<double, double>> &cities)
+{
     double total_dist = 0;
     int num_tour = tour.size();
 
-    for(int i = 0; i<num_tour; i++) {
-        total_dist += dist(tour[i], tour[(i+1)%num_tour], cities);
+    for (int i = 0; i < num_tour; i++)
+    {
+        total_dist += dist(tour[i], tour[(i + 1) % num_tour], cities);
     }
     return total_dist;
 }
 
-
 // Save tour to file filename in a .dot file format
-void save_tour(vector<int>& tour, vector<pair<double, double> >& cities, string filename) {
+void save_tour(vector<int> &tour, vector<pair<double, double>> &cities, string filename)
+{
     ofstream myfile;
-    myfile.open (filename);
+    myfile.open(filename);
     myfile << "strict digraph {\nlabel=\"" << filename << "\";" << endl;
     myfile << "node [shape=plaintext, fontcolor=red, fixedsize=true, width=0.05];" << endl;
 
     // Add nodes
-    for (int i = 0; i < tour.size(); i++) {
+    for (int i = 0; i < tour.size(); i++)
+    {
         myfile << i << " [pos=\"" << cities[i].first << "," << cities[i].second << "!\"];" << endl;
     }
 
     // Add edges
-    for (int i = 0; i < tour.size(); i++) {
+    for (int i = 0; i < tour.size(); i++)
+    {
         int from = tour[i];
-        int to = tour[(i+1) % tour.size()];
+        int to = tour[(i + 1) % tour.size()];
         myfile << from << " -> " << to << " [label=\"" << dist(from, to, cities) << "\"];" << endl;
     }
 
@@ -64,19 +68,22 @@ void save_tour(vector<int>& tour, vector<pair<double, double> >& cities, string 
     return;
 }
 
-
 // Find naive tour
-void getNaiveTour(int num_cities, vector<pair<double, double> >& cities, vector<int>& tour) {
+void getNaiveTour(int num_cities, vector<pair<double, double>> &cities, vector<int> &tour)
+{
     vector<bool> used;
     used.resize(num_cities, false);
     int best;
 
     tour[0] = 0;
     used[0] = true;
-    for (int i = 1; i < num_cities; i++) {
+    for (int i = 1; i < num_cities; i++)
+    {
         best = -1;
-        for (int j = 0; j < num_cities; j++) {
-            if (!used[j] && (best == -1 || dist(tour[i - 1], j, cities) < dist(tour[i - 1], best, cities))) {
+        for (int j = 0; j < num_cities; j++)
+        {
+            if (!used[j] && (best == -1 || dist(tour[i - 1], j, cities) < dist(tour[i - 1], best, cities)))
+            {
                 best = j;
             }
         }
@@ -85,76 +92,86 @@ void getNaiveTour(int num_cities, vector<pair<double, double> >& cities, vector<
     }
 }
 
-
 // Reverse elements in a tour from index "start" to index "end" (inclusive)
-void swap_tour(vector<int>& tour, int start, int end){
+void swap_tour(vector<int> &tour, int start, int end)
+{
     int temp;
 
     // Make sure bounds are correct
-    if (start > end) {
+    if (start > end)
+    {
         temp = start;
         start = end;
         end = temp;
     }
 
     // Swap elements pair by pair between [start,...,end]
-    for(int i= 0; i < floor((end - start + 1) / 2); i++) {
+    for (int i = 0; i < floor((end - start + 1) / 2); i++)
+    {
         // swap tour[start+i] and tour[end-i]
-        temp = tour[start+i];
-        tour[start+i] = tour[end-i];
-        tour[end-i] = temp;
+        temp = tour[start + i];
+        tour[start + i] = tour[end - i];
+        tour[end - i] = temp;
     }
 }
 
-
-
-void twoOpt(vector<int>& tour, int num_cities, vector<pair<double, double> >& cities) {   
+void twoOpt(vector<int> &tour, int num_cities, vector<pair<double, double>> &cities)
+{
     int improvements = 1;
     int counter = 0;
 
-    
-    label: if (counter > 30 || improvements == 0) return;
+label:
+    if (counter > 30 || improvements == 0)
+        return;
     improvements = 0;
 
     // Iterate over all cities to find swapping improvements
-    for (int i = 0; i < num_cities-1; i++) {
-        for (int j = i+1; j < num_cities; j++) {
-            if (j == i+1 || i == ((j+1) % num_cities)) continue; // Nodes are neighbors so swap is meaningless
+    for (int i = 0; i < num_cities - 1; i++)
+    {
+        for (int j = i + 1; j < num_cities; j++)
+        {
+            if (j == i + 1 || i == ((j + 1) % num_cities))
+                continue; // Nodes are neighbors so swap is meaningless
 
-            double edge1 = dist(tour[i],   tour[i+1], cities);                  // dist(c1,c2)
-            double edge2 = dist(tour[j],   tour[(j+1) % num_cities], cities);   // dist(c3,c4)
-            double edge3 = dist(tour[i],   tour[j], cities);                    // dist(c1,c3)
-            double edge4 = dist(tour[i+1], tour[(j+1) % num_cities], cities);   // dist(c2,c4)
-            if (edge1 + edge2 >  edge3 + edge4) { // Only swap if it will result in a shorter tour
-                swap_tour(tour, i+1, j);
+            double edge1 = dist(tour[i], tour[i + 1], cities);                    // dist(c1,c2)
+            double edge2 = dist(tour[j], tour[(j + 1) % num_cities], cities);     // dist(c3,c4)
+            double edge3 = dist(tour[i], tour[j], cities);                        // dist(c1,c3)
+            double edge4 = dist(tour[i + 1], tour[(j + 1) % num_cities], cities); // dist(c2,c4)
+            if (edge1 + edge2 > edge3 + edge4)
+            { // Only swap if it will result in a shorter tour
+                swap_tour(tour, i + 1, j);
                 improvements += 1;
                 //save_tour(tour, cities, "./graphs/tour" + to_string(counter) + ".dot");
-            } 
+            }
         }
     }
     counter += 1;
     goto label;
 }
 
-bool sortByDist(const tuple<int, int, float>& a, 
-               const tuple<int, int, float>& b)
+bool sortByDist(const tuple<int, int, float> &a,
+                const tuple<int, int, float> &b)
 {
     return (get<2>(a) < get<2>(b));
 }
 
-vector<tuple<int, int, float> > getEdges(vector<pair<double, double> >& cities, int num_cities) {
+vector<tuple<int, int, float>> getEdges(vector<pair<double, double>> &cities, int num_cities)
+{
     vector<tuple<int, int, float>> edges;
     int to_reserve = 0;
-    for (int i = num_cities-1; i > 0; i--) {
+    for (int i = num_cities - 1; i > 0; i--)
+    {
         to_reserve += i;
     }
     cout << "to_reserve: " << to_reserve << endl;
     edges.reserve(to_reserve);
-    vector<tuple<int, int, float> >::iterator it;
+    vector<tuple<int, int, float>>::iterator it;
     it = edges.begin();
-    for (int i = 0; i < num_cities-1; i++) {
-        for (int j = i+1; j < num_cities; j++) {
-            tuple<int,int,float> edge = make_tuple(i, j, dist(i, j, cities));
+    for (int i = 0; i < num_cities - 1; i++)
+    {
+        for (int j = i + 1; j < num_cities; j++)
+        {
+            tuple<int, int, float> edge = make_tuple(i, j, dist(i, j, cities));
             edges.insert(it, edge);
         }
     }
@@ -162,62 +179,68 @@ vector<tuple<int, int, float> > getEdges(vector<pair<double, double> >& cities, 
     return edges;
 }
 
-bool checkAndMergeClusters(set<set<int> > vertexClusters, int v1, int v2) {
-    bool foundV = false;
-    set<int> firstCluster;
-    for (set<set<int> >::iterator clusterIt = vertexClusters.begin(); clusterIt != vertexClusters.end(); ++clusterIt) {
+int find_cluster(int i, int *vertexClusters)
+{
+    if (i == vertexClusters[i])
+    {
+        return i;
+    }
+    return find_cluster(vertexClusters[i], vertexClusters);
+}
 
-        if ((*clusterIt).find(v1) != (*clusterIt).end()) { // Found v1
-            if (firstCluster.size() != 0) { // Has already found v2
-                // Merge v1 and v2 clusters and delete the other cluster.
-                firstCluster.insert((*clusterIt).begin(), (*clusterIt).end());
-                vertexClusters.erase(*clusterIt);
-                return true;
-            }
-            if ((*clusterIt).find(v2) == (*clusterIt).end()) { // Did not find v2
-                firstCluster = *clusterIt;
-            } else { // Found v2 in the same set
-                return false;
-            }
-        }
-        if ((*clusterIt).find(v2) == (*clusterIt).end()) { // Found v2
-            if (firstCluster.size() != 0) { // Has already found v1
-                // Merge v1 and v2 clusters and delete the other cluster.
-                firstCluster.insert((*clusterIt).begin(), (*clusterIt).end());
-                vertexClusters.erase(*clusterIt);
-                return true;
-            }
-            if ((*clusterIt).find(v1) != (*clusterIt).end()) { // Did not find v1
-                firstCluster = *clusterIt;
-            } else { // Found v1 in the same set
-                return false;
-            }
-        }
-        
+bool contains(vector<int> v, int x) {
+    if(std::find(v.begin(), v.end(), x) != v.end()) {
+        return true;
     }
     return false;
 }
 
+void printGraph(vector<vector<int> > v) {
+    int i = 0;
+    vector<vector<int>>::iterator row;
+    vector<int>::iterator col;
+    for (row = v.begin(); row != v.end(); row++)
+    {
+        cout << "VERTEX " << i << ": ";
+        for (col = row->begin(); col != row->end(); col++)
+        {
+            cout << (*col) << " ";
+        }
+        cout << endl;
+        i += 1;
+    }
+}
 
 /* returns a vector of vectors of ints. This represents a list of vertices, where each of them has a list of edges that attach them to the MST */
-vector<vector<int> > kruskals(vector<pair<double, double> >& cities, int num_cities) {
-    vector<tuple<int, int, float> > edges = getEdges(cities, num_cities);
-    /*
+vector<vector<int>> kruskals(vector<pair<double, double>> &cities, int num_cities)
+{
+    // -------------------------------Kruskals-----------------------------------
+
+    vector<tuple<int, int, float>> edges = getEdges(cities, num_cities);
+    
     // Print edges
     cout << "EDGES: " << endl;
     cout << to_string(edges.size()) << endl;
     for (int i = 0; i < edges.size(); i++) {
         cout << "(" << to_string(get<0>(edges[i])) << ", " << to_string(get<1>(edges[i])) << ", " << to_string(get<2>(edges[i])) << ")" << endl;
     }
-    cout << endl;*/
+    cout << endl;
 
-    set<set<int> > vertexClusters({});
-    for (int index = 0; index < num_cities; index++) {
+    //vector<vector<int> > vertexClusters({});
+    vector<vector<int>> mst{};
+    mst.reserve(num_cities);
+    int *vertexClusters = new int[num_cities];
+    for (int index = 0; index < num_cities; index++)
+    {
+        vertexClusters[index] = index;
+
+        vector<int> innerVector;
+        mst.push_back(innerVector);
         //cout << "insert " << index << endl;
-        set<int> cluster;
-        cluster.insert(index);
-        vertexClusters.insert(cluster);
-    } 
+        //set<int> cluster;
+        //cluster.insert(index);
+        //vertexClusters.insert(cluster);
+    }
 
     /*
     // Print vertexClusters
@@ -228,34 +251,145 @@ vector<vector<int> > kruskals(vector<pair<double, double> >& cities, int num_cit
         cout << endl;
     }*/
 
-    vector<vector<int> > mst{};
-    vector<vector<int> >::iterator it;
+    /*     vector<vector<int> >::iterator row = mst.begin();
+    vector<vector<int> >::iterator col = row.begin(); */
     bool merge = false;
-    for (tuple<int, int, float> edge : edges) {
-        merge = checkAndMergeClusters(vertexClusters, get<0>(edge), get<1>(edge));
+    for (tuple<int, int, float> edge : edges)
+    {
+
+        int cluster1 = find_cluster(get<0>(edge), vertexClusters);
+        int cluster2 = find_cluster(get<1>(edge), vertexClusters);
+
+        if (cluster1 != cluster2)
+        {
+            // Insert into MST
+            //cout << "type: " << typeid(get<0>(edge)).name() << endl;
+            //cout << get<0>(mst);
+
+            mst[get<0>(edge)].push_back(get<1>(edge));
+            mst[get<1>(edge)].push_back(get<0>(edge));
+
+            vertexClusters[cluster1] = vertexClusters[cluster2]; // Merge
+
+            for (int i = 0; i < num_cities; i++)
+            {
+                cout << "VERTEX " << i << ": " << vertexClusters[i] << endl;
+            }
+        }
+
+        /* merge = checkAndMergeClusters(vertexClusters, get<0>(edge), get<1>(edge));
+        cout << "edge: (" << get<0>(edge) << ", " << get<1>(edge) << ", " << get<2>(edge) << ")" << endl;
         if (merge) {
-            cout << "edge: (" << get<0>(edge) << ", " << get<1>(edge) << ", " << get<2>(edge) << ")" << endl;
             
-            /*it = get<get<0>(edge)>(mst).begin();
+             it = get<get<0>(edge)>(mst).begin();
             get<(get<0>(edge))>(mst).insert(it, get<1>(edge));
             it = get<get<1>(edge)>(mst).begin();
-            get<get<1>(edge)>(mst).insert(it, get<0>(edge)); */
+            get<get<1>(edge)>(mst).insert(it, get<0>(edge));  
+        } */
+    }
+
+    // ------------------------------ MST found ---------------------------------------------
+
+    cout << "--------------------------" << endl;
+    cout << "MST: " << endl;
+    printGraph(mst);
+
+    
+
+    // -------------------------------Find minimal matching-----------------------------------
+
+    // ha lista med odd degree vertices
+    vector<int> oddVertices;
+    oddVertices.reserve(num_cities); // Could be improved
+    for (int i = 0; i < mst.size(); i++) {
+        if (mst[i].size() % 2 != 0) { // Odd degree
+            oddVertices.push_back(i);
         }
     }
+
+    // gå igenom edges kortast -> längst
+    for (size_t i = 0; i < edges.size(); i++)
+    {
+        if (contains(oddVertices, get<0>(edges[i])) && contains(oddVertices, get<1>(edges[i]))) {
+            // lägg till den i mst
+            mst[get<0>(edges[i])].push_back(get<1>(edges[i]));
+            mst[get<1>(edges[i])].push_back(get<0>(edges[i]));
+
+            // ta bort elementen ur listan med odd degree vertices  
+            oddVertices.erase(find(oddVertices.begin(), oddVertices.end(), get<0>(edges[i])));
+            oddVertices.erase(find(oddVertices.begin(), oddVertices.end(), get<1>(edges[i])));
+        }
+    }
+
+    cout << "--------------------------" << endl;
+    cout << "Minimal matching: " << endl;
+    printGraph(mst);
+
+    // ------------------------Find Euler and remove visited already nodes---------------------
+
+    // Copy mst
+    vector<vector<int> > mstCopy;
+    mstCopy.reserve(num_cities);
+    for (int i = 0; i < mst.size(); i++) {
+        mstCopy[i].reserve(mst[i].size());
+        for (int j = 0; j < mst[i].size(); j++) {
+            mstCopy[i].push_back(mst[i][j]);
+        } 
+    }    
+
+
+
+    stack<int> stack;
+	int pos = start;
+    vector<int> path;
+	path.push_back(start);
+    while(!stack.empty() || mstCopy[pos].size() > 0){
+		//Current node has no neighbors
+		if(mstCopy[pos].empty()){
+			//add to circuit
+			path.push_back(pos);
+			//remove last vertex from stack and set it to current
+			pos = stack.top();
+			stack.pop();
+		}
+		//If current node has neighbors
+		else{
+			//Add vertex to stack
+			stack.push(pos);
+			//Take a neighbor
+			int neighbor = tempList[pos].back();
+			//Remove edge between neighbor and current vertex
+			tempList[pos].pop_back();
+			for(int i = 0; i < tempList[neighbor].size(); i++){
+				if(tempList[neighbor][i] == pos){
+					tempList[neighbor].erase(tempList[neighbor].begin()+i);
+				}
+			}
+			//Set neighbor as current vertex
+			pos = neighbor;
+		}
+	}
+	path.push_back(pos);
+
+    // Börja på nod 0
+    // Gå igenom alla noder som euler väg
+    // Prioritera alltid dubbelvägar
+    // 
+
     return mst;
 }
 
 int main()
 {
-    
+
     std::cout << std::fixed;
     std::cout << std::setprecision(10);
-    
+
     int num_cities;
     if (cin >> num_cities)
     {
         // Get input
-        vector<pair<double, double> > cities;
+        vector<pair<double, double>> cities;
         cities.reserve(num_cities);
         for (int i = 0; i < num_cities; i++)
         {
@@ -274,7 +408,7 @@ int main()
         // Get initial tour
         vector<int> initial_tour;
         initial_tour.resize(num_cities);
-        
+
         kruskals(cities, num_cities);
         //getNaiveTour(num_cities, cities, initial_tour);
         //double naive_dist = total_distance(initial_tour, cities);
@@ -283,9 +417,7 @@ int main()
         //twoOpt(initial_tour, num_cities, cities);
         //double new_dist = total_distance(initial_tour, cities);
 
-
-
-        cout << "---------------------------------ANSWER---------------------------------" << endl; 
+        cout << "---------------------------------ANSWER---------------------------------" << endl;
         // Print answer
         for (int i = 0; i < num_cities; i++)
         {
--- TSP-project/run-tests.sh
+++ TSP-project/run-tests.sh
@@ -6,10 +6,11 @@ g++ -std=c++0x greedy-2-opt.cpp -o tsp.o &&
 mkfifo fifo
 for i in {1..1}
 do
-    ./tester.o 1 > fifo & 
-    time ./tsp.o < fifo
+    #./tester.o 1 > fifo & 
+    #time ./tsp.o < fifo
+    time ./tsp.o < ./test3.in
 done
-rm fifo\
+#rm fifo\
 
 
 # do some stuff here
